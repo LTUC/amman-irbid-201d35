9597a7c844ff4ff9896b16a8cddeb57d6f7232e7
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-08-12 10:39:01 +0300

    demo and lecture about errors

---
+++ class-10/lecture/app.js
@@ -0,0 +1,54 @@
+'use strict';
+// let userName;//undefined
+// console.log('hi ' + userName);
+
+// console.log(shihab);
+
+// let num1 = 10;
+// let num1 = 20;
+// console.log(num1);
+
+// for { let i = 0; i< 10; i++} (
+//     let num1 = i;
+// console.log(num1);
+// )
+
+// let car = 'bmw,kia';
+// car.push('kia');
+// let carType = car.split(',');
+// console.log(carType);
+// let num1 = 12;
+// let result = num1.split(',');
+// console.log(result);
+
+// let grade = [];
+// let arrayLenght = grade.length = -1;
+// console.log(arrayLenght);
+
+// let grade = [];
+// let arrayLenght = grade.length = 99 ** 99;
+// console.log(arrayLenght);
+
+// const color = 'red';
+// color = 'blue';
+// console.log(color);
+
+// let num3 = 10;
+
+// function calc() {
+//     let num4 = 20;
+//     console.log(num3);
+//     return num4;
+// }
+// calc();
+// console.log(calc());
+// console.log(num4);
+
+// let num5 = 'a' / 3;
+// console.log(num5);
+// console.log(calc);
+// function calc {
+//     let num4 = 20;
+//     console.log(num3);
+//     return num4;
+// }
